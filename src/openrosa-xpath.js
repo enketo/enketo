8ee639943c6ae806545139fd86c4344ed2fd1bb3
Alex Anderson <anderson@medicmobile.org>
2015-07-13 16:36:38 +0200

    Add UUID generation

--- src/openrosa-xpath.js
+++ src/openrosa-xpath.js
@@ -1,8 +1,21 @@
-var openrosa_xpath = function() {
+var openrosa_xpath = function(e) {
+  _uuid_part = function(c) {
+      var r = Math.random()*16|0,
+              v=c=='x'?r:r&0x3|0x8;
+      return v.toString(16);
+  },
+  uuid = function() {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+              .replace(/[xy]/g, _uuid_part);
+  };
+
+  if(e === 'uuid()') {
+    return uuid();
+  }
 };
 
 if(typeof define === 'function') {
-	define(function() { return openrosa_xpath; });
+  define(function() { return openrosa_xpath; });
 } else if(typeof module === 'object' && typeof module.exports === 'object') {
-	module.exports = openrosa_xpath;
+  module.exports = openrosa_xpath;
 }
--- test/openrosa-xpath.spec.js
+++ test/openrosa-xpath.spec.js
@@ -9,7 +9,17 @@ define(['src/openrosa-xpath', 'chai'], function(openrosa_xpath, chai) {
 
   describe('openrosa-xpath', function() {
     it('should provide a function', function() {
-      assert.ok(openrosa_xpath);
+      assert.typeOf(openrosa_xpath, 'function');
+    });
+
+    describe('#uuid()', function() {
+      it('should provide an RFC 4122 version 4 compliant UUID string', function() {
+        // when
+        var provided = openrosa_xpath('uuid()');
+
+        // then
+        assert.match(provided, /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
+      });
     });
   });
 });
