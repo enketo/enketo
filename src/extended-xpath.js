e31df9bfe5084e0d2e4c6dec99d95ed0e7a61218
Alex Anderson <anderson@medicmobile.org>
2015-08-06 15:41:04 +0200

    Add new parser.

---
+++ src/extended-xpath.js
@@ -0,0 +1,163 @@
+var ExtendedXpathEvaluator = function(wrapped, extendedFuncs) {
+    var
+        callFn = function(name, args) {
+            if(extendedFuncs.hasOwnProperty(name)) {
+                return callExtended(name, args);
+            }
+            return callNative(name, args);
+        },
+        callExtended = function(name, args) {
+            var argVals = [], argString, res, i;
+            for(i=0; i<args.length; ++i) argVals.push(args[i].v);
+            return extendedFuncs[name].apply(null, argVals);
+        },
+        callNative = function(name, args) {
+            var argString = '', arg, quote, i;
+            for(i=0; i<args.length; ++i) {
+                arg = args[i];
+                if(arg.t !== 'num') {
+                    quote = arg.v.indexOf('"') === -1 ? '"' : "'";
+                    argString += quote;
+                }
+                argString += arg.v;
+                if(arg.t !== 'num') argString += quote;
+                if(i < args.length - 1) argString += ', ';
+            }
+            return wrapped({ t:'str', v:name + '(' + argString + ')' });
+        },
+    ___end_vars___;
+
+    this.evaluate = function(input) {
+        var cur, stack = [{ t:'root', tokens:[] }],
+            peek = function() { return stack.slice(-1)[0]; },
+            err = function(message) { throw new Error((message||'') + ' [stack=' + JSON.stringify(stack) + '] [cur=' + JSON.stringify(cur) + ']'); },
+            newCurrent = function() { cur = { t:'?', v:'' }; },
+            backtrack = function() {
+                // handle infix operators
+                var res, len, tokens, lhs, rhs, op;
+                tokens = peek().tokens;
+                len = tokens.length;
+                if(len >= 3) {
+                    lhs = tokens[len - 3];
+                    op  = tokens[len - 2];
+                    rhs = tokens[len - 1];
+
+                    if(     op.t === '+') res = lhs.v + rhs.v;
+                    else if(op.t === '-') res = lhs.v - rhs.v;
+                    else if(op.t === '*') res = lhs.v * rhs.v;
+                    else if(op.t === '/') res = lhs.v / rhs.v;
+                    else if(op.t === '%') res = lhs.v % rhs.v;
+
+                    if(typeof res !== 'undefined') {
+                        tokens = tokens.slice(0, -3);
+                        tokens.push({ t:'str', v:res });
+                        peek().tokens = tokens;
+                    }
+                }
+            },
+            handleXpathExpr = function() {
+                peek().tokens.push(wrapped(cur));
+                newCurrent();
+                backtrack();
+            },
+            lastChar = function() {
+                if(i > 0) return input.charAt(i-1);
+            },
+            nextChar = function() {
+                if(i < input.length -1) return input.charAt(i+1);
+            },
+            ___end_vars___;
+
+        newCurrent();
+
+        for(i=0; i<input.length; ++i) {
+            c = input.charAt(i);
+            if(cur.t === 'str') {
+                if(c === cur.quote) {
+                    peek().tokens.push(cur);
+                    backtrack();
+                    newCurrent();
+                } else cur.v += c;
+            } else switch(c) {
+                case "'":
+                case '"':
+                    if(cur.t === '?' && cur.v === '') {
+                        cur = { t:'str', quote:c, v:'' };
+                    } else err('Not sure how to handle: ' + c);
+                    break;
+                case '(':
+                    if(cur.t === '?' && cur.v !== '') {
+                        cur.t = 'fn';
+                        cur.tokens = [];
+                        stack.push(cur);
+                        newCurrent();
+                    } else err();
+                    break;
+                case ')':
+                    if(cur.t === '?') {
+                        if(cur.v !== '') handleXpathExpr();
+                        var fn = stack.pop();
+                        if(fn.t !== 'fn') err();
+                        peek().tokens.push(callFn(fn.v, fn.tokens));
+                        backtrack();
+                        newCurrent();
+                    } else err();
+                    break;
+                case ',':
+                    if(cur.t === '?') {
+                        if(cur.v !== '') handleXpathExpr();
+                        if(peek().t !== 'fn') err();
+                    } else err();
+                    break;
+                case '-':
+                    if(/[0-9]/.test(nextChar()) ||
+                            (nextChar() !== ' ' && lastChar() !== ' ')) {
+                        // -ve number or function name expr
+                        cur.v += c;
+                        break;
+                    } // else it's `-` operator
+                    /* falls through */
+                case '+':
+                case '*':
+                    if(cur.t === '?') {
+                        if(cur.v !== '') handleXpathExpr();
+                        peek().tokens.push({ t:c });
+                    } else err();
+                    break;
+                case ' ':
+                    if(cur.t === '?') {
+                        if(cur.v !== '') {
+                            if(cur.v === 'mod') {
+                                peek().tokens.push({ t:'%' });
+                                newCurrent();
+                            } else if(cur.v === 'div') {
+                                peek().tokens.push({ t:'/' });
+                                newCurrent();
+                            } else {
+                                handleXpathExpr();
+                            }
+                        }
+                        break;
+                    }
+                    /* falls through */
+                default:
+                    cur.v += c;
+            }
+        }
+
+        if(cur.t === '?' && cur.v !== '') handleXpathExpr();
+
+        if(cur.t !== '?' || cur.v !== '' || (cur.tokens && current.tokens.length)) err('Current item not evaluated!');
+        if(stack.length > 1) err('Stuff left on stack.');
+        if(stack[0].t !== 'root') err('Weird stuff on stack.');
+        if(stack[0].tokens.length === 0) err('No tokens.');
+        if(stack[0].tokens.length > 1) err('Too many tokens.');
+        return stack[0].tokens[0].v;
+    };
+};
+
+if(typeof define === 'function') {
+  define(function() { return ExtendedXpathEvaluator; });
+} else if(typeof module === 'object' && typeof module.exports === 'object') {
+  module.exports = ExtendedXpathEvaluator;
+}
---
+++ test/extended-xpath.spec.js
@@ -0,0 +1,134 @@
+define(['src/extended-xpath', 'chai', 'lodash'], function(ExtendedXpathEvaluator, chai, _) {
+  var DATE_MATCH = '(Thu) (Aug) \\d\\d 2015 \\d\\d:\\d\\d:\\d\\d GMT([+-]\\d\\d\\d\\d \(.+\))?',
+      examples = {
+        '"double-string"':
+          /^double-string$/,
+        "'single-string'":
+          /^single-string$/,
+        '"string(shhh)"':
+            /^string\(shhh\)$/,
+        'date()':
+            new RegExp('^' + DATE_MATCH + '$'),
+        'upcase("spOtTy")':
+            /^SPOTTY$/,
+        'concat("single")':
+            /^single$/,
+        'concat("a","b")':
+            /^ab$/,
+        'concat("a","b","c")':
+            /^abc$/,
+        'concat("a", "b", "c")':
+            /^abc$/,
+        '"plus" + "one"':
+            /^plusone$/,
+        '"plus" + "one" + "plus" + "two"':
+            /^plusoneplustwo$/,
+        'upcase("what") + upcase("ever")':
+            /^WHATEVER$/,
+        'downcase("Fox"+"Trot")':
+            /^foxtrot$/,
+        'downcase("Fox" + "Trot")':
+            /^foxtrot$/,
+        'concat(date())':
+            new RegExp('^' + DATE_MATCH + '$'),
+        'concat(date(), "X")':
+            new RegExp('^' + DATE_MATCH + 'X$'),
+        'concat("X", date())':
+            new RegExp('^X' + DATE_MATCH + '$'),
+        '"Today\'s date: " + date()':
+            new RegExp('^Today\'s date: ' + DATE_MATCH + '$'),
+        'concat("Report::", "Today\'s date: " + date())':
+            new RegExp('^Report::Today\'s date: ' + DATE_MATCH + '$'),
+        'concat(concat(upcase("Big") + downcase("Little")) + "Average", " by ", concat("Some", " ", "Author"))':
+            /^BIGlittleAverage by Some Author$/,
+        '/xpath/expression':
+            /^<xpath:\/xpath\/expression>$/,
+        '"string-prefix-" + /xpath/expression':
+            /^string-prefix-<xpath:\/xpath\/expression>$/,
+        '/xpath/expression + "-string-suffix"':
+            /^<xpath:\/xpath\/expression>-string-suffix$/,
+        'concat("Evaluates to: ", /xpath/expression)':
+            /^Evaluates to: <xpath:\/xpath\/expression>$/,
+        '3':
+            /^3$/,
+        '3.1416':
+            /^3.1416$/,
+        '-3':
+            /^-3$/,
+        '-3.1416':
+            /^-3.1416$/,
+        '1 + 1':
+            /^2$/,
+        '1 - 1':
+            /^0$/,
+        '10 div 100':
+            /^0.1$/,
+        'random()':
+            /^0.\d+$/,
+        'random() div 10':
+            /^0.0\d+$/,
+        '12 mod 5':
+            /^2$/,
+        'reverse("hello " + "friend")':
+            /^dneirf olleh$/,
+        'reverse("hello ") + reverse("friend")':
+            /^ ollehdneirf$/,
+        'native_function()':
+            /^<xpath:native_function\(\)>$/,
+        'native_function(3)':
+            /^<xpath:native_function\(3\)>$/,
+        'native_function("string-arg")':
+            /^<xpath:native_function\("string-arg"\)>$/,
+        'native_function(\'string-with-escaped-"-arg\')':
+            /^<xpath:native_function\('string-with-escaped-"-arg'\)>$/,
+        'native_function(1, 2, 3, "a", \'b\', "c")':
+            /^<xpath:native_function\(1, 2, 3, "a", "b", "c"\)>$/,
+        'native-function()':
+            /^<xpath:native-function\(\)>$/,
+        'native-function(3)':
+            /^<xpath:native-function\(3\)>$/,
+        'native-function("string-arg")':
+            /^<xpath:native-function\("string-arg"\)>$/,
+        'native-function(1, 2, 3, "a", \'b\', "c")':
+            /^<xpath:native-function\(1, 2, 3, "a", "b", "c"\)>$/,
+        'native-function1(native-function2() + native-function3()) + native-function4(native-function5() + native-function6())':
+            /^<xpath:native-function1\("<xpath:native-function2\(\)><xpath:native-function3\(\)>"\)><xpath:native-function4\("<xpath:native-function5\(\)><xpath:native-function6\(\)>"\)>$/,
+      },
+      xp = {
+        str: function(v) { return { t:'str', v:v }; },
+        num: function(v) { return { t:'num', v:v }; },
+      },
+      extendedXpathEvaluator = new ExtendedXpathEvaluator(
+        function wrappedXpathEvaluator(xpath) {
+          // TODO actually evaluate
+          var v = xpath.v.trim();
+          var regChec = /^-?[0-9]+(\.[0-9]+)?$/.test(v);
+          if(regChec) {
+            return { t:'num', v:parseFloat(v) };
+          }
+          return { t:'str', v:'<xpath:' + xpath.v + '>' };
+        },
+        {
+          upcase: function(it) { return xp.str(it.toUpperCase()); },
+          downcase: function(it) { return xp.str(it.toLowerCase()); },
+          date: function() { return xp.str(new Date().toString()); },
+          concat: function() {
+            var i, acc = '';
+            for(i=0; i<arguments.length; ++i) acc += arguments[i];
+            return xp.str(acc);
+          },
+          random: function() { return xp.num(Math.random()); },
+          reverse: function(it) { return xp.str(it.split('').reverse().join('')); },
+        }
+      );
+
+  describe('ExtendedXpathEvaluator', function() {
+    _.collect(examples, function(expected, expr) {
+      it(expr + ' should be evaluated', function() {
+        assert.match(
+          extendedXpathEvaluator.evaluate(expr),
+          expected);
+      });
+    });
+  });
+});
