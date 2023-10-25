f352456c4fdc6f6e6c461417d5c48bdeb7981ece
eyelidlessness <gnosis@gmail.com>
2023-10-25 15:21:14 -0700

    Convert create_config.py to Node script

--- packages/enketo-express/setup/docker/start.sh
+++ packages/enketo-express/setup/docker/start.sh
@@ -5,7 +5,7 @@ source /etc/profile
 
 cd ${ENKETO_SRC_DIR}/
 
-# Create a config. file if necessary.
-python setup/docker/create_config.py
+# Create a config file if necessary.
+node setup/docker/create-config.js
 
 node app.js -n enketo
