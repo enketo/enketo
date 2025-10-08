ef7535c8dc18b9d18a486c839eb82b56fd59b0f4
Paulo Amorim <pauloamorimbr@gmail.com>
2025-10-08 09:34:35 -0300

    Change JSDoc

--- packages/enketo-core/src/js/widget.js
+++ packages/enketo-core/src/js/widget.js
@@ -220,7 +220,7 @@ class Widget {
     }
 
     /**
-     * Sets a custom validation error message on the widget's element.
+     * Sets a custom validation error message on the widget's element if it exists.
      * If a message is provided, it sets the 'data-error-message' attribute on the element.
      * If no message is provided, it clears any existing error message.
      *
