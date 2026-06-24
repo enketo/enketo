bbb58f44205d275c278c5e687af605e5f0beb00f
Paulo Amorim <pauloamorimbr@gmail.com>
2026-06-24 17:05:40 -0300

    fix(autocomplete): replace fragment creation with element creation for fake input

--- packages/enketo-core/src/widget/select-autocomplete/autocomplete.js
+++ packages/enketo-core/src/widget/select-autocomplete/autocomplete.js
@@ -70,13 +70,12 @@ class AutocompleteSelectpicker extends Widget {
             }
         });
 
-        const fragment = document
-            .createRange()
-            .createContextualFragment(
-                `<input type="text" class="ignore widget autocomplete" list="${listId}" />`
-            );
+        const fakeInput = document.createElement('input');
+        fakeInput.type = 'text';
+        fakeInput.className = 'ignore widget autocomplete';
+        fakeInput.setAttribute('list', listId);
         this.element.classList.add('hide');
-        this.element.after(fragment);
+        this.element.after(fakeInput);
 
         this.fakeInput =
             this.element.parentElement.querySelector('input.autocomplete');
