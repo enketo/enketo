691896618a084ced9d0d68714c2aeda1bcd466f7
Paulo Amorim <pauloamorimbr@gmail.com>
2026-06-29 12:57:47 -0300

    Apply review sugestions

--- packages/enketo-core/test/spec/widget.autocomplete.spec.js
+++ packages/enketo-core/test/spec/widget.autocomplete.spec.js
@@ -4,7 +4,7 @@ import { testStaticProperties } from '../helpers/test-widget';
 testStaticProperties(AutocompleteWidget);
 
 describe('AutocompleteWidget security', () => {
-    it('does not inject HTML attributes from a malicious list ID (DEV-2341)', (done) => {
+    it('does not inject HTML attributes from a malicious list ID (DEV-2341)', () => {
         // A list attribute value containing a double-quote and event handler,
         // as would result from a hostile XForm nodeset after transformation.
         const maliciousListId = '" onclick="alert(1)';
@@ -28,17 +28,12 @@ describe('AutocompleteWidget security', () => {
             );
         }
 
-        Promise.resolve()
-            .then(() => new AutocompleteWidget(control))
-            .then((widget) => {
-                const fakeInput =
-                    widget.question.querySelector('input.autocomplete');
-                // The fake input must exist and must NOT have an injected onclick attribute.
-                expect(fakeInput).to.not.equal(null);
-                expect(fakeInput.hasAttribute('onclick')).to.equal(false);
-                // The list attribute must be present.
-                expect(fakeInput.hasAttribute('list')).to.equal(true);
-            })
-            .then(done, done);
+        const widget = new AutocompleteWidget(control);
+        const fakeInput = widget.question.querySelector('input.autocomplete');
+        // The fake input must exist and must NOT have an injected onclick attribute.
+        expect(fakeInput).to.not.equal(null);
+        expect(fakeInput.hasAttribute('onclick')).to.equal(false);
+        // The list attribute must be present.
+        expect(fakeInput.hasAttribute('list')).to.equal(true);
     });
 });
