dee9eabfdd8c121cdb50e2e79c824e48e05d034e
Paulo Amorim <pauloamorimbr@gmail.com>
2026-08-10 00:38:33 -0300

    fix(core): update Form.requiredTransformerVersion to 4.4.0

--- packages/enketo-core/src/js/form.js
+++ packages/enketo-core/src/js/form.js
@@ -1610,6 +1610,6 @@ Form.prototype.goToTarget = function (target, options = {}) {
  * @type {string}
  * @default
  */
-Form.requiredTransformerVersion = '4.3.1';
+Form.requiredTransformerVersion = '4.4.0';
 
 export { Form, FormModel };
